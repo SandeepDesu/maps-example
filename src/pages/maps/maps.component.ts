9078f46266edcd2bc2f799998b50d6f133506e80
sandy <dsandy.ip@gmail.com>
2017-10-13 03:30:15 -0700

    change version of openlayers

--- src/pages/maps/maps.component.ts
+++ src/pages/maps/maps.component.ts
@@ -1,12 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import ol_Map from 'ol/map';
-import ol_layer_vector from 'ol/layer/Vector';
-import ol_layer_Tile from 'ol/layer/tile';
-import ol_source_OSM from 'ol/source/osm';
-import ol_source_vector from 'ol/source/vector';
-import ol_View from 'ol/view';
-import ol_proj from 'ol/proj';
-import ol_format_GeoJSON from 'ol/format/GeoJSON'
+import ol from 'openlayers';
+
 import { MapService } from './../../services/mapservice.services';
 @Component({
     selector: 'map-root',
@@ -31,24 +25,24 @@ export class MapComponent implements OnInit {
             );
     }
     successHandler(data) {
-        var vector = new ol_layer_vector({
-            source: new ol_source_vector({
+        var vector = new ol.layer.vector({
+            source: new ol.source.vector({
              })
           });
-          var map = new ol_Map({
+          var map = new ol.Map({
             layers: [
-            new ol_layer_Tile({
-                      source: new ol_source_OSM()
+            new ol.layer.Tile({
+                      source: new ol.source.OSM()
                     }),
                      vector ],
-                      view: new ol_View({
-                           center: ol_proj.transform([-95.68542201, 50], 'EPSG:4326', 'EPSG:3857'),
+                      view: new ol.View({
+                           center: ol.proj.transform([-95.68542201, 50], 'EPSG:4326', 'EPSG:3857'),
                            zoom: 3
                           }),
                       target: 'map'
           });
           
-          var format = new ol_format_GeoJSON();
+          var format = new ol.format.GeoJSON();
           var myGeoJsonFeatures = format.readFeatures(
           data, 
           {featureProjection: 'EPSG:3857'}
--- src/pages/pointer/pointer.component.ts
+++ src/pages/pointer/pointer.component.ts
@@ -1,17 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MapService } from './../../services/mapservice.services';
-import ol_Map from 'ol/Map';
-import ol_View from 'ol/View';
-import ol_proj from 'ol/proj';
-import ol_Feature from 'ol/Feature';
-import ol_geom_Point from 'ol/geom/Point';
-import ol_source_Vector from 'ol/source/Vector';
-import ol_source_OSM from 'ol/source/OSM';
-import ol_layer_Vector from 'ol/layer/Vector';
-import ol_layer_Tile from 'ol/layer/Tile';
-import ol_style_Style from 'ol/style/style';
-import ol_style_Icon from 'ol/style/icon';
-import ol_Overlay from 'ol/overlay';
+import ol from 'openlayers';
+
 import _ from 'lodash';
 
 @Component({
@@ -21,11 +11,11 @@ import _ from 'lodash';
 })
 
 export class MapPointer implements OnInit {
-  constructor(public mapService: MapService) {}
   statesJson: any;
   sourceData: any;
   regionData: any;
   merge = [];
+  constructor(public mapService: MapService) {}
   ngOnInit() {
     this.mapService.getVisableData().subscribe(data => {
       this.sourceData = data;
@@ -46,13 +36,13 @@ export class MapPointer implements OnInit {
       _.forEach(this.statesJson, (v, k) => {
         if (key === v[0]) {
           this.mapService.getCordinates(v[1]).subscribe(coordinates => {
-            var iconFeature = new ol_Feature({
-              geometry: new ol_geom_Point(ol_proj.fromLonLat([coordinates.results[0].geometry.location.lng, coordinates.results[0].geometry.location.lat])),
+            var iconFeature = new ol.Feature({
+              geometry: new ol.geom.Point(ol.proj.fromLonLat([coordinates.results[0].geometry.location.lng, coordinates.results[0].geometry.location.lat])),
               city: v[1],
               value: value.tpAccRat
             });
-            iconFeature.setStyle(new ol_style_Style({
-              image: new ol_style_Icon(/** @type {olx.style.IconOptions} */({
+            iconFeature.setStyle(new ol.style.Style({
+              image: new ol.style.Icon(/** @type {olx.style.IconOptions} */({
                 color: '#8959A8',
                 crossOrigin: 'anonymous',
                 src: 'https://openlayers.org/en/v4.3.4/examples/data/dot.png' //https://openlayers.org/en/v3.12.1/examples/data/icon.png
@@ -76,32 +66,32 @@ export class MapPointer implements OnInit {
     var container = document.getElementById('popup');
     var content = document.getElementById('popup-content');
     var closer = document.getElementById('popup-closer');
-    var overlay = new ol_Overlay(/** @type {olx.OverlayOptions} */({
+    var overlay = new ol.Overlay(/** @type {olx.OverlayOptions} */({
       element: container,
       autoPan: true,
       autoPanAnimation: {
         duration: 250
       }
     }));
-    var vectorSource = new ol_source_Vector({
+    var vectorSource = new ol.source.Vector({
       features: this.merge
     });
 
-    var vectorLayer = new ol_layer_Vector({
+    var vectorLayer = new ol.layer.Vector({
       source: vectorSource
     });
 
-    var map = new ol_Map({
+    var map = new ol.Map({
       layers: [
-        new ol_layer_Tile({
-          source: new ol_source_OSM()
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
         }), vectorLayer
       ],
       target: 'map-one',
       overlays: [overlay],
       controls: [],
-      view: new ol_View({
-        center: ol_proj.transform([-90.68542201, 38], 'EPSG:4326', 'EPSG:3857'),
+      view: new ol.View({
+        center: ol.proj.transform([-90.68542201, 38], 'EPSG:4326', 'EPSG:3857'),
         zoom: 4.5
       })
     });
